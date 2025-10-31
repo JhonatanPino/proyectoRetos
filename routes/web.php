9bb9be50a2999be48e8b66352e130c5aa0460ccb
JhonatanPino <jhonatanpinorivera@gmail.com>
2025-10-30 19:23:38 -0500

    modify file public/index.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('app');
-// });
+Route::get('/', function () {
+    return view('app');
+});
 
 Route::get('/{any}', function () {
     return view('app');
