7e9c79a6cc1435cd9a73bbbfba8da54f8e5c02c7
JhonatanPino <jhonatanpinorivera@gmail.com>
2025-11-04 15:48:06 -0500

    config for deploy

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://luxury-sopapillas-0092e5.netlify.app'],
 
-    'allowed_origins_patterns' => [''],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('app');
-});
-
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '^(?!api).*$'); // ❗️Ignora rutas que empiecen con /api
-
 Route::get('/health', function () {
     return response()->json(['status' => 'ok']);
-});
-
-Route::get('/', function () {
-    return response()->json(['message' => 'Backend operativo ✅']);
 });
