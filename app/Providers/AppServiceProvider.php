e95ba63aefcfd04573ba57910ac7fd366dbfc486
JhonatanPino <jhonatan_pino01171@elpoli.edu.co>
2026-09-07 09:17:04 -0500

    configure function in providers and cors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function () {
+        RateLimiter::for('api', function ( Request $request) {
             return Limit::none(); // 👈 Desactiva el límite temporalmente para probar
             // O amplíalo si prefieres: return Limit::perMinute(100)->by($request->ip());
         });
